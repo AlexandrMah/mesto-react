039e273267e1529ecc9ae36e604bdf835191c2a3
Aleksandr Makhin <machin.alexandr@mail.ru>
2023-06-23 12:07:48 +0300

    сделать правки по 1 ревью 1

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -6,7 +6,7 @@ function ImagePopup(props) {
       <div className="popup__container-image">
         <img src={props.card.link} alt={props.card.nameImg} className="popup__image"/>
         <h2 className="popup__name-image">{props.card.nameImg}</h2>
-        <button onClick = {props.onClose} type="button" className="popup__close-btn popup__close-btn_open-image"></button>
+        <button onClick = {props.onClose} type="button" className="popup__close-btn popup__close-btn_open-image"/>
       </div>
     </section>
   )
